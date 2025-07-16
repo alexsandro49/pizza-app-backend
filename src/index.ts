0eefea46151e22f5d2a9a17a91706431f58083bd
Alex Sandro <alexsandro49.dev@gmail.com>
2025-07-16 00:20:21 -0300

    feat: user route

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import express from 'express'
+import { userRoutes } from './user/user'
 
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json())
+app.use("/", userRoutes)
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`)
