35ebb07a7584064b085954c4fdf6b29a250815f0
Alex Sandro <alexsandro49.dev@gmail.com>
2025-07-18 21:34:57 -0300

    feat: auth logic

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express'
+import cors from "cors"
 import { authRoutes } from './routes/auth'
 
 const app = express()
 const port = 3000
 
 app.use(express.json())
+app.use(cors())
+
 app.use("/", authRoutes)
 
 app.listen(port, () => {
--- src/prisma.ts
+++ src/prisma.ts
@@ -3,9 +3,9 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 prisma.$use(async (params, next) => {
-  if (params.action === 'create' && params.model === 'User') {
-    const crypto = await import('crypto');
+  const crypto = await import('crypto');
 
+  if (params.action === 'create' && params.model === 'User') {
     const idHash = crypto.createHash('sha256').update(`${Date.now()}-${Math.random()}`)
         .digest('hex');
     const passwordHash = crypto.createHash('sha256').update(params.args.data.password)
@@ -13,6 +13,11 @@ prisma.$use(async (params, next) => {
 
     params.args.data.id = idHash;
     params.args.data.password = passwordHash;
+  } else if (params.action === 'findUniqueOrThrow' && params.model === 'User') {
+    const passwordHash = crypto.createHash('sha256').update(params.args.where.password)
+      .digest('hex');
+
+    params.args.where.password = passwordHash;
   }
 
   return next(params);
